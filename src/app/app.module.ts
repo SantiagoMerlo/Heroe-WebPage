a49c719d6fc138c567e4cf1b1cc9953739efe74a
SAntiagoMerlo <santiago.agustin.merlo@gmail.com>
2019-09-20 13:32:20 -0300

    :)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './componets/shader/navbar/navbar.component';
+import { HomeComponent } from './componets/home/home.component';
+import { AboutComponent } from './componets/about/about.component';
+import { HeroesComponent } from './componets/heroes/heroes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule
